b1aeef9a1f4105bd86f4e21cb44723a4cd412122
pistack <pistatex@yonsei.ac.kr>
2022-06-08 00:11:45 +0900

    fit_tscan: 1. add fix irf option to fix irf parameter during fitting 2. add contribution table of each lifetime component and remove component plot add fit_irf utilty fit_irf is useful if one can directly measure irf function

--- src/TRXASprefitpack/__init__.py
+++ src/TRXASprefitpack/__init__.py
@@ -11,5 +11,5 @@ from .thy import *
 from .data_process import *
 from .mathfun import *
 
-__version__ = '0.4.9'
+__version__ = '0.4.10'
 
--- src/TRXASprefitpack/tools/fit_irf.py
+++ src/TRXASprefitpack/tools/fit_irf.py
@@ -1,6 +1,6 @@
-# fit tscan
-# fitting tscan data
-# Using sum of exponential decay convolved with
+# fit irf
+# fitting irf function
+# There are three irf shapes are avaliable
 # normalized gaussian distribution
 # normalized cauchy distribution
 # normalized pseudo voigt profile
@@ -9,109 +9,48 @@
 
 import argparse
 import numpy as np
-from ..mathfun import model_n_comp_conv, fact_anal_exp_conv
+from ..mathfun import gau_irf, cauchy_irf, pvoigt_irf
 from lmfit import Parameters, fit_report, minimize
 import matplotlib.pyplot as plt
 
 
-def fit_tscan():
+def fit_irf():
 
-    def set_bound_tau(tau):
-        bound = [tau/2, 1]
-        if 0.1 < tau <= 10:
-            bound = [0.05, 100]
-        elif 10 < tau <= 100:
-            bound = [5, 500]
-        elif 100 < tau <= 1000:
-            bound = [50, 2000]
-        elif 1000 < tau <= 5000:
-            bound = [500, 10000]
-        elif 5000 < tau <= 50000:
-            bound = [2500, 100000]
-        elif 50000 < tau <= 500000:
-            bound = [25000, 1000000]
-        elif 500000 < tau <= 1000000:
-            bound = [250000, 2000000]
-        elif 1000000 < tau:
-            bound = [tau/2, np.inf]
-        return bound
-
-    def residual(params, t, num_comp, base, irf, data=None, eps=None):
+    def residual(params, t, irf, data=None, eps=None):
         if irf in ['g', 'c']:
             fwhm = params['fwhm']
         else:
             fwhm = np.array([params['fwhm_G'], params['fwhm_L']])
-        tau = np.zeros(num_comp)
-        for i in range(num_comp):
-            tau[i] = params[f'tau_{i+1}']
         chi = np.zeros((data.shape[0], data.shape[1]))
         for i in range(data.shape[1]):
             t0 = params[f't_0_{i+1}']
-            c = fact_anal_exp_conv(t-t0, fwhm, tau, irf=irf,
-                                   data=data[:, i], eps=eps[:, i], base=base)
-
-            chi[:, i] = data[:, i] - \
-                model_n_comp_conv(t-t0, fwhm, tau, c, base=base,
-                                  irf=irf)
+            if irf == 'g':
+                chi[:, i] = data[:, i] - gau_irf(t-t0, fwhm)
+            elif irf == 'c':
+                chi[:, i] = data[:, i] - cauchy_irf(t-t0, fwhm)
+            else:
+                f = fwhm[0]**5+2.69269*fwhm[0]**4*fwhm[1]+2.42843*fwhm[0]**3*fwhm[1]**2 + \
+                    4.47163*fwhm[0]**2*fwhm[1]**3 + 0.07842*fwhm[0]*fwhm[1]**4 + fwhm[1]**5
+                f = f**(1/5)
+                eta = 1.36603*(fwhm[1]/f)-0.47719*(fwhm[1]/f)**2+0.11116*(fwhm[1]/f)**3
+                chi[:, i] = data[:, i] - pvoigt_irf(t-t0, fwhm[0], fwhm[1], eta)
         chi = chi.flatten()/eps.flatten()
 
         return chi
 
-    description = 'fit tscan: fitting tscan data ' + \
-        'using sum of exponential decay covolved with ' + \
-        'gaussian/cauchy(lorenzian)/pseudo voigt irf function ' + \
-        'it uses fact_anal_exp_conv to determine best ' + \
-        'c_i\'s when timezero, fwhm, ' + \
-        'and time constants are given. ' + \
-        'So, to use this script what you need to ' + \
-        'give are only timezero, fwhm, and time constants ' + \
+    description = 'fit irf: fitting instrumental response function ' + \
+        'There are three irf shapes are avaliable. ' + \
+        '1. gaussian 2. cauchy(lorenzian) 3. pseudo voigt shape' + \
         'To set boundary of each parameter for fitting, ' + \
         'I use following scheme.' + '\n'*2 + \
         '''
-*fwhm: temporal width of x-ray pulse
+*fwhm: temporal width of pulse
 lower bound: 0.5*fwhm_init
 upper bound: 2*fwhm_init
 
-*t_0: timezero for each scan
+*t_0: timezero or center of irf function for each scan
 lower bound: t_0 - 2*fwhm_init
 upper bound: t_0 + 2*fwhm_init
-
-*tau: life_time of each component
-if tau < 0.1
-lower bound: tau/2
-upper bound: 1
-
-if 0.1 < tau < 10
-lower bound: 0.05
-upper bound: 100
-
-if 10 < tau < 100
-lower bound: 5
-upper bound: 500
-
-if 100 < tau < 1000
-lower bound: 50
-upper bound: 2000
-
-if 1000 < tau < 5000 then
-lower bound: 500
-upper bound: 10000
-
-if 5000 < tau < 50000 then
-lower bound: 2500
-upper bound: 100000
-
-if 50000 < tau < 500000 then
-lower bound: 25000
-upper bound: 1000000
-
-if 500000 < tau < 1000000 then
-lower bound: 250000
-upper bound: 2000000
-
-if 1000000 < tau then
-lower bound: tau/2
-upper bound: np.inf
 '''
 
     epilog = '''
@@ -120,9 +59,8 @@ upper bound: np.inf
 1. if you set shape of irf to pseudo voigt (pv), then
    you should provide two full width at half maximum
    value for gaussian and cauchy parts, respectively.
-
-2. If you did not set tau then it assume you finds the
-   timezero of this scan. So, --no_base option is discouraged.
+2. This script is only useful when one can directly measure
+   instrumental response function.
 '''
     tmp = argparse.RawDescriptionHelpFormatter
     parser = argparse.ArgumentParser(formatter_class=tmp,
@@ -152,12 +90,6 @@ upper bound: np.inf
                         help='time zeros for each tscan')
     parser.add_argument('-t0f', '--time_zeros_file',
                         help='filename for time zeros of each tscan')
-    parser.add_argument('--tau', type=float, nargs='*',
-                        help='lifetime of each component')
-    parser.add_argument('--no_base', action='store_false',
-                        help='exclude baseline for fitting')
-    parser.add_argument('--fix_irf', action='store_true',
-    help='fix irf parameter (fwhm_G, fwhm_L) during fitting process')
     parser.add_argument('--slow', action='store_true',
     help='use slower but robust global optimization algorithm')
     parser.add_argument('-o', '--out', default=None,
@@ -192,16 +124,6 @@ upper bound: np.inf
             fwhm = 0.5346*args.fwhm_L + \
                 np.sqrt(0.2166*args.fwhm_L**2+args.fwhm_G**2)
 
-    if args.tau is None:
-        find_zero = True  # time zero mode
-        base = True
-        num_comp = 0
-    else:
-        find_zero = False
-        tau = np.array(args.tau)
-        base = args.no_base
-        num_comp = tau.shape[0]
-
     if (args.time_zeros is None) and (args.time_zeros_file is None):
         print('You should set either time_zeros or time_zeros_file!\n')
         return
@@ -224,52 +146,39 @@ upper bound: np.inf
     fit_params = Parameters()
     if irf in ['g', 'c']:
         fit_params.add('fwhm', value=fwhm,
-                       min=0.5*fwhm, max=2*fwhm, vary=(not args.fix_irf))
+                       min=0.5*fwhm, max=2*fwhm)
     elif irf == 'pv':
         fit_params.add('fwhm_G', value=args.fwhm_G,
-                       min=0.5*args.fwhm_G, max=2*args.fwhm_G, vary=(not args.fix_irf))
+                       min=0.5*args.fwhm_G, max=2*args.fwhm_G)
         fit_params.add('fwhm_L', value=args.fwhm_L,
-                       min=0.5*args.fwhm_L, max=2*args.fwhm_L, vary=(not args.fix_irf))
+                       min=0.5*args.fwhm_L, max=2*args.fwhm_L)
     for i in range(num_scan):
         fit_params.add(f't_0_{i+1}', value=time_zeros[i],
                        min=time_zeros[i]-2*fwhm,
                        max=time_zeros[i]+2*fwhm)
 
-    if not find_zero:
-        for i in range(num_comp):
-            bd = set_bound_tau(tau[i])
-            fit_params.add(f'tau_{i+1}', value=tau[i], min=bd[0],
-                           max=bd[1])
-
     # Second initial guess using global optimization algorithm
     if args.slow: 
         out = minimize(residual, fit_params, method='ampgo',
-        args=(t, num_comp, base, irf),
+        args=(t, irf),
         kws={'data': data, 'eps': eps})
     else:
         out = minimize(residual, fit_params, method='nelder',
-        args=(t, num_comp, base, irf),
+        args=(t, irf),
         kws={'data': data, 'eps': eps})
 
     # Then do Levenberg-Marquardt
     out = minimize(residual, out.params,
-                   args=(t, num_comp, base),
-                   kws={'data': data, 'eps': eps, 'irf': irf})
+                   args=(t, irf),
+                   kws={'data': data, 'eps': eps})
 
-    chi2_ind = residual(out.params, t, num_comp, base,
-                        irf, data=data, eps=eps)
+    chi2_ind = residual(out.params, t, irf, data=data, eps=eps)
     chi2_ind = chi2_ind.reshape(data.shape)
     chi2_ind = np.sum(chi2_ind**2, axis=0)/(data.shape[0]-len(out.params))
 
     fit = np.zeros((data.shape[0], data.shape[1]+1))
     fit[:, 0] = t
-    tau_opt = np.zeros(num_comp)
-    for j in range(num_comp):
-        tau_opt[j] = out.params[f'tau_{j+1}']
-    if base:
-        c = np.zeros((num_comp+1, num_scan))
-    else:
-        c = np.zeros((num_comp, num_scan))
+
     for i in range(num_scan):
         if irf in ['g', 'c']:
             fwhm_out = out.params['fwhm']
@@ -277,54 +186,32 @@ upper bound: np.inf
             tmp_G = out.params['fwhm_G']
             tmp_L = out.params['fwhm_L']
             fwhm_out = np.array([tmp_G, tmp_L])
-        c[:, i] = fact_anal_exp_conv(t-out.params[f't_0_{i+1}'],
-                                     fwhm_out,
-                                     tau_opt,
-                                     data=data[:, i],
-                                     eps=eps[:, i],
-                                     base=base,
-                                     irf=irf).flatten()
-        fit[:, i+1] = model_n_comp_conv(t-out.params[f't_0_{i+1}'],
-                                        fwhm_out,
-                                        tau_opt,
-                                        c[:, i],
-                                        base=base,
-                                        irf=irf)
+        
+        if irf == 'g':
+            fit[:, i+1] = gau_irf(t-out.params[f't_0_{i+1}'],
+            fwhm_out)
+        elif irf == 'c':
+            fit[:, i+1] = cauchy_irf(t-out.params[f't_0_{i+1}'],
+            fwhm_out)
+        else:
+            f_out = fwhm_out[0]**5+2.69269*fwhm_out[0]**4*fwhm_out[1]+2.42843*fwhm_out[0]**3*fwhm_out[1]**2 + \
+                    4.47163*fwhm_out[0]**2*fwhm_out[1]**3 + 0.07842*fwhm_out[0]*fwhm_out[1]**4 + fwhm_out[1]**5
+            f_out = f_out**(1/5)
+            eta_out = 1.36603*(fwhm_out[1]/f_out)-0.47719*(fwhm_out[1]/f_out)**2+0.11116*(fwhm_out[1]/f_out)**3
+            fit[:, i+1] = pvoigt_irf(t-out.params[f't_0_{i+1}'], fwhm_out[0], fwhm_out[1], eta_out)
     
     f = open(out_prefix+'_fit_report.txt', 'w')
     f.write(fit_report(out))
     f.close()
 
     np.savetxt(out_prefix+'_fit.txt', fit)
-    np.savetxt(out_prefix+'_c.txt', c)
 
     print(fit_report(out))
 
-    c_abs = np.abs(c)
-    c_sum = np.sum(c_abs, axis=0)
-    c_table = np.zeros_like(c)
-    for i in range(num_scan):
-        c_table[:, i] = c[:, i]/c_sum[i]*100
-
-    table_print = '    '
-    for i in range(num_scan):
-        table_print = table_print + f'tscan {i+1} |'
-    table_print = table_print + '\n'
-    for i in range(num_comp):
-        table_print = table_print + '    '
-        for j in range(num_scan):
-            table_print = table_print + f'{c_table[i, j]:.2f} % |'
-        table_print = table_print + '\n'
-    
-    print('[[Component Contribution]]')
-    print(table_print)
 
     for i in range(num_scan):
         plt.figure(i+1)
         title = f'Chi squared: {chi2_ind[i]:.2f}'
-        if find_zero:
-            t0 = out.params[f't_0_{i+1}']
-            title = f'time_zero: {t0.value:.4e}\n' + title
         plt.title(title)
         plt.errorbar(t, data[:, i],
                      eps[:, i], marker='o', mfc='none',
