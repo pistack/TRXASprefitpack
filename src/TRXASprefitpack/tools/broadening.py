e5d83bbeb14590f3bd689a913b649d0b1700b108
pistack <pistack@yonsei.ac.kr>
2022-06-10 14:36:10 +0900

    change behavior of broadening to match fit_static

--- src/TRXASprefitpack/tools/broadening.py
+++ src/TRXASprefitpack/tools/broadening.py
@@ -23,28 +23,38 @@ def broadening():
                         help='Full Width at Half Maximum of gaussian shape')
     parser.add_argument('fwhm_L', type=float,
                         help='Full Width at Half Maximum of lorenzian shape')
-    parser.add_argument('peak_shift', type=float,
-                        help='discrepancy of peak position between theory ' +
-                        'and experiment')
-    parser.add_argument('-o', '--out', default='out',
-                        help='prefix for output files')
+    parser.add_argument('peak_factor', type=float,
+    help='parameter to match descrepency between thoretical spectrum and experimental spectrum')
+    parser.add_argument('--scale_energy', action='store_true',
+    help='Scaling the energy of peak instead of shifting to match experimental spectrum')
+    parser.add_argument('-o', '--out', help='prefix for output files')
     args = parser.parse_args()
 
     peak = np.genfromtxt(args.peak)
-    out = args.out
+    if args.out is None:
+        out = args.prefix
+    else:
+        out = args.out
     e_min = args.e_min
     e_max = args.e_max
     e_step = args.e_step
     A = args.A
     fwhm_G = args.fwhm_G
     fwhm_L = args.fwhm_L
-    peak_shift = args.peak_shift
+    peak_factor = args.peak_factor
+    if args.scale_energy:
+        policy = 'scale'
+    else:
+        policy = 'shift' 
     e = np.linspace(e_min, e_max, int((e_max-e_min)/e_step)+1)
 
-    broadened_thy = gen_theory_data(e, peak, A, fwhm_G, fwhm_L, peak_shift)
+    broadened_thy = gen_theory_data(e, peak, A, fwhm_G, fwhm_L, peak_factor, policy)
 
     rescaled_stk = peak
-    rescaled_stk[:, 0] = rescaled_stk[:,0]-peak_shift
+    if policy == 'scale':
+        rescaled_stk[:, 0] = rescaled_stk[:, 0]*peak_factor
+    else:
+        rescaled_stk[:, 0] = rescaled_stk[:, 0] - peak_factor
     rescaled_stk[:, 1] = A*rescaled_stk[:, 1]
     spec_thy = np.vstack((e, broadened_thy))
 
