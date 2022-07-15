fix gradient routine

src/TRXASprefitpack/mathfun/exp_conv_irf.py
@@ -215,12 +215,17 @@ def deriv_exp_conv_cauchy(t: Union[float, np.ndarray],
             grad[2] = -(t*f.imag+fwhm*f.real/2)/np.pi
     else:
         grad = np.empty((3, t.size))
-        z = (t+complex(0, fwhm/2))
-        f = exp1x(-k*z)
-        tmp = -(k*f+1/z)/np.pi
-        grad[0, :] = tmp.imag
-        grad[1, :] = tmp.real/2
-        grad[2, :] = -(t*f.imag+fwhm*f.real/2)/np.pi
+        if k == 0:
+            grad[0] = 2/(np.pi*fwhm*(1+(2*t/fwhm)**2))
+            grad[1] = -t/fwhm*grad[0]
+            grad[2] = 0
+        else:
+            z = (t+complex(0, fwhm/2))
+            f = exp1x(-k*z)
+            tmp = -(k*f+1/z)/np.pi
+            grad[0, :] = tmp.imag
+            grad[1, :] = tmp.real/2
+            grad[2, :] = -(t*f.imag+fwhm*f.real/2)/np.pi
     return grad
 
 # calculate derivative of the convolution of sum of exponential decay and instrumental response function

src/TRXASprefitpack/tools/fit_tscan.py
@@ -142,7 +142,7 @@ def fit_tscan():
             step = data[i].shape[0]
             for j in range(data[i].shape[1]):
                 t0 = params[t0_idx_curr]
-                A = make_A_matrix_exp(t[i]-t0, fwhm, tau, base, irf)
+                A = make_A_matrix_exp(t[i]-t0, fwhm, tau, base, irf, eta)
                 c = fact_anal_A(A, data[i][:,j], eps[i][:,j])
                 
                 if irf == 'g':
@@ -150,8 +150,8 @@ def fit_tscan():
                 elif irf == 'c':
                     grad = deriv_exp_sum_conv_cauchy(t[i]-t0, fwhm, 1/tau, c, base)
                 else:
-                    grad_gau = deriv_exp_sum_conv_gau(t[i]-t0, fwhm, 1/tau, c, base)
-                    grad_cauchy = deriv_exp_sum_conv_cauchy(t[i]-t0, fwhm, 1/tau, c, base)
+                    grad_gau = deriv_exp_sum_conv_gau(t[i]-t0, fwhm[0], 1/tau, c, base)
+                    grad_cauchy = deriv_exp_sum_conv_cauchy(t[i]-t0, fwhm[1], 1/tau, c, base)
                     grad = grad_gau + eta*(grad_cauchy-grad_gau)
    
                 grad = np.einsum('j,ij->ij', 1/eps[i][:, j], grad)