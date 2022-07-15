c2938d93e6fdefbe20518d19bc835c474a5c0487
pistack <pistack@yonsei.ac.kr>
2022-07-15 12:00:09 +0900

    implement derivative of convloution of exponential decay and cauchy irf

--- src/TRXASprefitpack/mathfun/exp_conv_irf.py
+++ src/TRXASprefitpack/mathfun/exp_conv_irf.py
@@ -11,6 +11,35 @@ from typing import Union, Optional
 import numpy as np
 from scipy.special import erfc, erfcx, wofz, exp1
 
+# special function: scaled expoential integral
+# if scipy starts to support scaled expoential integral I will delete this function 
+
+def exp1x_asymp(z: Union[complex, np.ndarray]) -> Union[complex, np.ndarray]:
+    return z*(1+z*(1+2*z*(1+3*z*(1+4*z*(1+5*z*(1+6*z*(1+7*z*(1+8*z*(1+9*z*(1+10*z))))))))))
+
+def exp1x(z: Union[complex, np.ndarray]) -> Union[complex, np.ndarray]:
+    
+    if not isinstance(z, np.ndarray):
+            if np.abs(z.real) < 200:
+                ans = np.exp(z)*exp1(z)
+                ans = complex(np.cos(z.imag), np.sin(z.imag))*exp1(z)
+                ans = np.exp(z.real)*ans
+            else:
+                ans = -exp1x_asymp(-1/z)
+    else:
+        mask = np.abs(z.real) < 200
+        inv_mask = np.invert(mask)
+        ans = np.empty(z.size, dtype=np.complex128)
+        
+        # abs(Re z) < 200
+        ans[mask] = (np.cos(z[mask].imag)+complex(0,1)*np.sin(z[mask].imag))*exp1(z[mask])
+        ans[mask] = np.exp(z.real[mask])*ans[mask]
+
+        # abs(Re z) > 200, use asymptotic series
+        ans[inv_mask] = -exp1x_asymp(-1/z[inv_mask])
+    return ans
+
+# calculate convolution of exponential decay and instrumental response function
 
 def exp_conv_gau(t: Union[float, np.ndarray], fwhm: float,
                  k: float) -> Union[float, np.ndarray]:
@@ -51,6 +80,65 @@ def exp_conv_gau(t: Union[float, np.ndarray], fwhm: float,
 
     return ans
 
+def exp_conv_cauchy(t: Union[float, np.ndarray],
+                    fwhm: float,
+                    k: float) -> Union[float, np.ndarray]:
+
+    '''
+    Compute exponential function convolved with normalized cauchy
+    distribution
+
+    Args:
+       t: time
+       fwhm: full width at half maximum of cauchy distribution
+       k: rate constant (inverse of life time)
+
+    Returns:
+      Convolution of normalized cauchy distribution and
+      exponential decay :math:`(\\exp(-kt))`.
+    '''
+
+    if k == 0:
+        ans = 0.5+1/np.pi*np.arctan(2*t/fwhm)
+    else:
+        z = -k*t - complex(0, k*fwhm/2)
+        ans = exp1x(z).imag/np.pi
+    return ans
+
+def exp_conv_pvoigt(t: Union[float, np.ndarray],
+                    fwhm_G: float,
+                    fwhm_L: float,
+                    eta: float,
+                    k: float) -> Union[float, np.ndarray]:
+
+    '''
+    Compute exponential function convolved with normalized pseudo
+    voigt profile (i.e. linear combination of normalized gaussian and
+    cauchy distribution)
+
+    :math:`\\eta C(\\mathrm{fwhm}_L, t) + (1-\\eta)G(\\mathrm{fwhm}_G, t)`
+
+    Args:
+       t: time
+       fwhm_G: full width at half maximum of gaussian part of
+               pseudo voigt profile
+       fwhm_L: full width at half maximum of cauchy part of
+               pseudo voigt profile
+       eta: mixing parameter
+       k: rate constant (inverse of life time)
+
+    Returns:
+       Convolution of normalized pseudo voigt profile and
+       exponential decay :math:`(\\exp(-kt))`.
+    '''
+
+    u = exp_conv_gau(t, fwhm_G, k)
+    v = exp_conv_cauchy(t, fwhm_L, k)
+
+    return u + eta*(v-u)
+
+# calculate derivative of the convolution of exponential decay and instrumental response function
+
 def deriv_exp_conv_gau(t: Union[float, np.ndarray], fwhm: float,
                  k: float) -> np.ndarray:
 
@@ -89,6 +177,54 @@ def deriv_exp_conv_gau(t: Union[float, np.ndarray], fwhm: float,
 
     return grad
 
+def deriv_exp_conv_cauchy(t: Union[float, np.ndarray],
+                    fwhm: float,
+                    k: float) -> Union[float, np.ndarray]:
+
+    '''
+    Compute derivative of the convolution of exponential function and normalized cauchy
+    distribution
+
+    Args:
+       t: time
+       fwhm: full width at half maximum of cauchy distribution
+       k: rate constant (inverse of life time)
+
+    Returns:
+      Derivative of convolution of normalized cauchy distribution and
+      exponential decay :math:`(\\exp(-kt))`.
+    
+    Note:
+     1st row: df/dt
+     2nd row: df/d(fwhm)
+     3rd row: df/dk
+    '''
+
+    if not isinstance(t, np.ndarray):
+        grad = np.empty(3)
+        if k == 0:
+            grad[0] = 2/(np.pi*fwhm*(1+(2*t/fwhm)**2))
+            grad[1] = -t/fwhm*grad[0]
+            grad[2] = 0
+        else:
+            z = (t+complex(0, fwhm/2))
+            f = exp1x(-k*z)
+            tmp = -(k*f+1/z)/np.pi
+            grad[0] = tmp.imag
+            grad[1] = tmp.real/2
+            grad[2] = -(t*f.imag+fwhm*f.real/2)/np.pi
+    else:
+        grad = np.empty((3, t.size))
+        z = (t+complex(0, fwhm/2))
+        f = exp1x(-k*z)
+        tmp = -(k*f+1/z)/np.pi
+        grad[0, :] = tmp.imag
+        grad[1, :] = tmp.real/2
+        grad[2, :] = -(t*f.imag+fwhm*f.real/2)/np.pi
+    return grad
+
+# calculate derivative of the convolution of sum of exponential decay and instrumental response function
+
 def deriv_exp_sum_conv_gau(t: np.ndarray, fwhm: float,
                  k: np.ndarray, c: np.ndarray, base: Optional[bool] = True) -> np.ndarray:
 
@@ -124,89 +260,40 @@ def deriv_exp_sum_conv_gau(t: np.ndarray, fwhm: float,
 
     return grad
 
-def exp1x_asymp(z: Union[complex, np.ndarray]) -> Union[complex, np.ndarray]:
-    return z*(1+z*(1+2*z*(1+3*z*(1+4*z*(1+5*z*(1+6*z*(1+7*z*(1+8*z*(1+9*z*(1+10*z))))))))))
-
-def exp_conv_cauchy(t: Union[float, np.ndarray],
-                    fwhm: float,
-                    k: float) -> Union[float, np.ndarray]:
+def deriv_exp_sum_conv_cauchy(t: np.ndarray, fwhm: float,
+                 k: np.ndarray, c: np.ndarray, base: Optional[bool] = True) -> np.ndarray:
 
     '''
-    Compute exponential function convolved with normalized cauchy
+    Compute derivative of sum of exponential function convolved with normalized cauchy
     distribution
 
     Args:
-       t: time
-       fwhm: full width at half maximum of cauchy distribution
-       k: rate constant (inverse of life time)
+      t: time
+      fwhm: full width at half maximum of cauchy distribution
+      k: rate constant (inverse of life time)
+      c: coefficient
+      base: include baseline (i.e. k=0)
 
     Returns:
-      Convolution of normalized cauchy distribution and
-      exponential decay :math:`(\\exp(-kt))`.
+     Derivative of Convolution of normalized cauchy distribution and sum of exponential
+     decay :math:`(\\exp(-kt))`.
+    Note:
+     1st row: df/dt
+     2nd row: df/d(fwhm)
+     2+i th row: df/dk_i
     '''
+    grad = np.zeros((2+k.size, t.size))
+    for i in range(k.size):
+        grad_i = deriv_exp_conv_cauchy(t, fwhm, k[i])
+        grad[0, :] = grad[0, :] + c[i]*grad_i[0, :]
+        grad[1, :] = grad[1, :] + c[i]*grad_i[1, :]
+        grad[2+i, :] = c[i]*grad_i[2, :]
+    if base:
+        grad_i = deriv_exp_conv_cauchy(t, fwhm, 0)
+        grad[0, :] = grad[0, :] + c[-1]*grad_i[0, :]
+        grad[1, :] = grad[1, :] + c[-1]*grad_i[1, :]
 
-    if k == 0:
-        ans = 0.5+1/np.pi*np.arctan(2*t/fwhm)
-    else:
-        kgamma = k*fwhm/2
-        ikgamma = complex(0, kgamma)
-        kt = k*t
-        if not isinstance(t, np.ndarray):
-            if np.abs(kt) < 200:
-                ans = complex(np.cos(kgamma), -np.sin(kgamma))*exp1(-kt-ikgamma)
-                ans = np.exp(-kt)*ans.imag/np.pi
-            else:
-                inv_z = 1/(kt+ikgamma)
-                ans = exp1x_asymp(inv_z)
-                ans = -ans.imag/np.pi
-        else:
-            mask = np.abs(kt) < 200
-            inv_mask = np.invert(mask)
-
-            ans = np.empty(t.size, dtype=np.float64)
-            inv_z = 1/(kt[inv_mask]+ikgamma)
-
-            # abs(kt) < 200
-            ans1 = complex(np.cos(kgamma), -np.sin(kgamma))*exp1(-kt[mask]-ikgamma)
-            ans[mask] = np.exp(-kt[mask])*ans1.imag/np.pi
-
-            # abs(kt) > 200, use asymptotic series
-            ans2 = exp1x_asymp(inv_z)
-            ans[inv_mask] = -ans2.imag/np.pi
-    return ans
-
-
-def exp_conv_pvoigt(t: Union[float, np.ndarray],
-                    fwhm_G: float,
-                    fwhm_L: float,
-                    eta: float,
-                    k: float) -> Union[float, np.ndarray]:
-
-    '''
-    Compute exponential function convolved with normalized pseudo
-    voigt profile (i.e. linear combination of normalized gaussian and
-    cauchy distribution)
-
-    :math:`\\eta C(\\mathrm{fwhm}_L, t) + (1-\\eta)G(\\mathrm{fwhm}_G, t)`
-
-    Args:
-       t: time
-       fwhm_G: full width at half maximum of gaussian part of
-               pseudo voigt profile
-       fwhm_L: full width at half maximum of cauchy part of
-               pseudo voigt profile
-       eta: mixing parameter
-       k: rate constant (inverse of life time)
-
-    Returns:
-       Convolution of normalized pseudo voigt profile and
-       exponential decay :math:`(\\exp(-kt))`.
-    '''
-
-    u = exp_conv_gau(t, fwhm_G, k)
-    v = exp_conv_cauchy(t, fwhm_L, k)
-
-    return u + eta*(v-u)
+    return grad
 
 def dmp_osc_conv_gau(t: Union[float, np.ndarray], fwhm: float,
 k: float, T: float, phase: float) -> Union[float, np.ndarray]:
