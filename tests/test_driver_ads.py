3ff9c80513d2d1a491cacbcbce79c13fbfe66b21
pistack <pistack@yonsei.ac.kr>
2022-08-18 18:56:29 +0900

    add more test plan: implement test case for ads driver routine

---
+++ tests/test_driver_ads.py
@@ -0,0 +1,29 @@
+# pylint: disable = missing-module-docstring, wrong-import-position, invalid-name
+import os
+import sys
+import numpy as np
+
+path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(path+'/../src/')
+
+from TRXASprefitpack import exp_conv, solve_seq_model, rate_eq_conv
+from TRXASprefitpack import sads, dads
+
+def test_driver_sads_1():
+    return
+
+def test_driver_sads_2():
+    return
+
+def test_driver_sads_3():
+    return
+
+def test_driver_dads_1():
+    return
+
+def test_driver_dads_2():
+    return
+
+def test_driver_dads_3():
+    return
+
--- tests/test_fact_anal.py
+++ tests/test_fact_anal.py
@@ -12,7 +12,7 @@ from TRXASprefitpack import sum_exp_dmp_osc_conv, fact_anal_sum_exp_dmp_osc_conv
 
 
 
-def test_fact_anal_exp_conv():
+def test_fact_anal_exp_conv_1():
     fwhm = 0.15
     c_ref = np.array([1, -0.5, -0.25])
     tau = np.array([0.3, 10])
@@ -23,7 +23,31 @@ def test_fact_anal_exp_conv():
     c_tst = fact_anal_exp_conv(t, fwhm, tau, True, irf='g', intensity=expt, eps=eps)
     assert np.allclose(c_ref, c_tst)
 
-def test_fact_anal_dmp_osc_conv():
+def test_fact_anal_exp_conv_2():
+    fwhm = 0.15
+    c_ref = np.array([1, -0.5, -0.25])
+    tau = np.array([0.3, 10])
+    t = np.hstack((np.arange(-1, 1, 0.05), np.linspace(1, 100, 99)))
+    model = exp_conv(t, fwhm, tau, c_ref, True, irf='c')
+    eps = np.ones_like(model)
+    expt = model
+    c_tst = fact_anal_exp_conv(t, fwhm, tau, True, irf='c', intensity=expt, eps=eps)
+    assert np.allclose(c_ref, c_tst)
+
+def test_fact_anal_exp_conv_3():
+    fwhm = 0.10
+    eta = 0.3
+    c_ref = np.array([1, -0.5, -0.25])
+    tau = np.array([0.3, 10])
+    t = np.hstack((np.arange(-1, 1, 0.05), np.linspace(1, 100, 99)))
+    model = exp_conv(t, fwhm, tau, c_ref, True, irf='pv', eta=eta)
+    eps = np.ones_like(model)
+    expt = model
+    c_tst = fact_anal_exp_conv(t, fwhm, tau, True, irf='pv', eta=eta,
+    intensity=expt, eps=eps)
+    assert np.allclose(c_ref, c_tst)
+
+def test_fact_anal_dmp_osc_conv_1():
     fwhm = 0.15
     c_ref = np.array([1, 0.5])
     tau = np.array([0.3, 1])
@@ -38,7 +62,39 @@ def test_fact_anal_dmp_osc_conv():
     assert np.allclose(c_ref, c_tst)
     assert np.allclose(phase, phase_tst)
 
-def test_fact_anal_sum_exp_dmp_osc_conv():
+def test_fact_anal_dmp_osc_conv_2():
+    fwhm = 0.15
+    c_ref = np.array([1, 0.5])
+    tau = np.array([0.3, 1])
+    period = np.array([0.5, 1])
+    phase = np.array([-np.pi/17, 2*np.pi/4])
+    t = np.hstack((np.arange(-1, 1, 0.02), np.linspace(1, 100, 99)))
+    model = dmp_osc_conv(t, fwhm, tau, period, phase, c_ref, irf='c')
+    eps = np.ones_like(model)
+    expt = model
+    phase_tst, c_tst = fact_anal_dmp_osc_conv(t, fwhm, tau, period, irf='c', intensity=expt, eps=eps)
+
+    assert np.allclose(c_ref, c_tst)
+    assert np.allclose(phase, phase_tst)
+
+def test_fact_anal_dmp_osc_conv_3():
+    fwhm = 0.10
+    eta = 0.3
+    c_ref = np.array([1, 0.5])
+    tau = np.array([0.3, 1])
+    period = np.array([0.5, 1])
+    phase = np.array([-np.pi/17, 2*np.pi/4])
+    t = np.hstack((np.arange(-1, 1, 0.02), np.linspace(1, 100, 99)))
+    model = dmp_osc_conv(t, fwhm, tau, period, phase, c_ref, irf='pv', eta=eta)
+    eps = np.ones_like(model)
+    expt = model
+    phase_tst, c_tst = fact_anal_dmp_osc_conv(t, fwhm, tau, period, irf='pv', eta=eta, 
+    intensity=expt, eps=eps)
+
+    assert np.allclose(c_ref, c_tst)
+    assert np.allclose(phase, phase_tst)
+
+def test_fact_anal_sum_exp_dmp_osc_conv_1():
     fwhm = 0.15
     c_ref_decay = np.array([1, -0.5, -0.25])
     c_ref_osc = np.array([0.15, 0.075])
@@ -54,6 +110,47 @@ def test_fact_anal_sum_exp_dmp_osc_conv():
     c_tst_decay, phase_tst_osc, c_tst_osc = \
         fact_anal_sum_exp_dmp_osc_conv(t, fwhm, tau, tau_osc, period_osc, base=True,
         irf='g', intensity=expt, eps=eps)
+    assert np.allclose(c_ref_decay, c_tst_decay)
+    assert np.allclose(c_ref_osc, c_tst_osc)
+    assert np.allclose(phase_osc, phase_tst_osc)
+
+def test_fact_anal_sum_exp_dmp_osc_conv_2():
+    fwhm = 0.15
+    c_ref_decay = np.array([1, -0.5, -0.25])
+    c_ref_osc = np.array([0.15, 0.075])
+    tau = np.array([0.5, 10])
+    tau_osc = np.array([0.3, 1])
+    period_osc = np.array([0.5, 1])
+    phase_osc = np.array([np.pi/3, -np.pi/4])
+    t = np.hstack((np.arange(-1, 1, 0.02), np.linspace(1, 100, 99)))
+    model = sum_exp_dmp_osc_conv(t, fwhm, tau, tau_osc, period_osc, phase_osc,
+    c_ref_decay, c_ref_osc, base=True, irf='c')
+    eps = np.ones_like(model)
+    expt = model
+    c_tst_decay, phase_tst_osc, c_tst_osc = \
+        fact_anal_sum_exp_dmp_osc_conv(t, fwhm, tau, tau_osc, period_osc, base=True,
+        irf='c', intensity=expt, eps=eps)
+    assert np.allclose(c_ref_decay, c_tst_decay)
+    assert np.allclose(c_ref_osc, c_tst_osc)
+    assert np.allclose(phase_osc, phase_tst_osc)
+
+def test_fact_anal_sum_exp_dmp_osc_conv_3():
+    fwhm = 0.10
+    eta = 0.3
+    c_ref_decay = np.array([1, -0.5, -0.25])
+    c_ref_osc = np.array([0.15, 0.075])
+    tau = np.array([0.5, 10])
+    tau_osc = np.array([0.3, 1])
+    period_osc = np.array([0.5, 1])
+    phase_osc = np.array([np.pi/3, -np.pi/4])
+    t = np.hstack((np.arange(-1, 1, 0.02), np.linspace(1, 100, 99)))
+    model = sum_exp_dmp_osc_conv(t, fwhm, tau, tau_osc, period_osc, phase_osc,
+    c_ref_decay, c_ref_osc, base=True, irf='pv', eta=eta)
+    eps = np.ones_like(model)
+    expt = model
+    c_tst_decay, phase_tst_osc, c_tst_osc = \
+        fact_anal_sum_exp_dmp_osc_conv(t, fwhm, tau, tau_osc, period_osc, base=True,
+        irf='pv', eta=eta, intensity=expt, eps=eps)
     assert np.allclose(c_ref_decay, c_tst_decay)
     assert np.allclose(c_ref_osc, c_tst_osc)
     assert np.allclose(phase_osc, phase_tst_osc)
