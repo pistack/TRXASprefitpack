0f8a58c1b5eed98e8e8bf04456279fcd31e54c51
pistack <pistatex@yonsei.ac.kr>
2024-02-09 03:08:42 +0900

    implement hessian for pseudo voigt parameter function fwhm and eta

--- tests/test_irf.py
+++ tests/test_irf.py
@@ -6,9 +6,10 @@ import numpy as np
 path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(path+"/../src/")
 
-from TRXASprefitpack.mathfun.deriv_check import check_num_deriv
+from TRXASprefitpack.mathfun.deriv_check import check_num_deriv, check_num_hess
 from TRXASprefitpack import calc_eta, deriv_eta
 from TRXASprefitpack import calc_fwhm, deriv_fwhm
+from TRXASprefitpack import hess_fwhm_eta
 from TRXASprefitpack import pvoigt_irf
 from TRXASprefitpack import voigt
 
@@ -162,4 +163,77 @@ def test_deriv_fwhm_6():
 
     assert np.allclose(np.array([d_G, d_L]),
     check_num_deriv(calc_fwhm, fwhm_G, fwhm_L))
+
+def test_hess_fwhm_eta_1():
+    fwhm_G = 0.1
+    fwhm_L = 0.3
+    Hess_fwhm_tst, Hess_eta_tst = hess_fwhm_eta(fwhm_G, fwhm_L)
+    Hess_fwhm_ref = check_num_hess(calc_fwhm, fwhm_G, fwhm_L)
+    Hess_eta_ref = check_num_hess(calc_eta, fwhm_G, fwhm_L)
+
+    assert np.allclose(Hess_eta_ref.flatten()[[0,1,3]], 
+                       Hess_eta_tst)
+    assert np.allclose(Hess_fwhm_ref.flatten()[[0,1,3]], 
+                       Hess_fwhm_tst)
+
+def test_hess_fwhm_eta_2():
+    fwhm_G = 0.1
+    fwhm_L = 0.15
+    Hess_fwhm_tst, Hess_eta_tst = hess_fwhm_eta(fwhm_G, fwhm_L)
+    Hess_fwhm_ref = check_num_hess(calc_fwhm, fwhm_G, fwhm_L)
+    Hess_eta_ref = check_num_hess(calc_eta, fwhm_G, fwhm_L)
+
+    assert np.allclose(Hess_eta_ref.flatten()[[0,1,3]], 
+                       Hess_eta_tst)
+    assert np.allclose(Hess_fwhm_ref.flatten()[[0,1,3]], 
+                       Hess_fwhm_tst)
+
+def test_hess_fwhm_eta_3():
+    fwhm_G = 0.1
+    fwhm_L = 0.1
+    Hess_fwhm_tst, Hess_eta_tst = hess_fwhm_eta(fwhm_G, fwhm_L)
+    Hess_fwhm_ref = check_num_hess(calc_fwhm, fwhm_G, fwhm_L)
+    Hess_eta_ref = check_num_hess(calc_eta, fwhm_G, fwhm_L)
+
+    assert np.allclose(Hess_eta_ref.flatten()[[0,1,3]], 
+                       Hess_eta_tst)
+    assert np.allclose(Hess_fwhm_ref.flatten()[[0,1,3]], 
+                       Hess_fwhm_tst)
+
+def test_hess_fwhm_eta_4():
+    fwhm_G = 0.15
+    fwhm_L = 0.1
+    Hess_fwhm_tst, Hess_eta_tst = hess_fwhm_eta(fwhm_G, fwhm_L)
+    Hess_fwhm_ref = check_num_hess(calc_fwhm, fwhm_G, fwhm_L)
+    Hess_eta_ref = check_num_hess(calc_eta, fwhm_G, fwhm_L)
+
+    assert np.allclose(Hess_eta_ref.flatten()[[0,1,3]], 
+                       Hess_eta_tst)
+    assert np.allclose(Hess_fwhm_ref.flatten()[[0,1,3]], 
+                       Hess_fwhm_tst)
+
+def test_hess_fwhm_eta_5():
+    fwhm_G = 0.3
+    fwhm_L = 0.1
+    Hess_fwhm_tst, Hess_eta_tst = hess_fwhm_eta(fwhm_G, fwhm_L)
+    Hess_fwhm_ref = check_num_hess(calc_fwhm, fwhm_G, fwhm_L)
+    Hess_eta_ref = check_num_hess(calc_eta, fwhm_G, fwhm_L)
+
+    assert np.allclose(Hess_eta_ref.flatten()[[0,1,3]], 
+                       Hess_eta_tst)
+    assert np.allclose(Hess_fwhm_ref.flatten()[[0,1,3]], 
+                       Hess_fwhm_tst)
+    
+def test_hess_fwhm_eta_6():
+    fwhm_G = 0.15
+    fwhm_L = 0.3
+    Hess_fwhm_tst, Hess_eta_tst = hess_fwhm_eta(fwhm_G, fwhm_L)
+    Hess_fwhm_ref = check_num_hess(calc_fwhm, fwhm_G, fwhm_L)
+    Hess_eta_ref = check_num_hess(calc_eta, fwhm_G, fwhm_L)
+
+    assert np.allclose(Hess_eta_ref.flatten()[[0,1,3]], 
+                       Hess_eta_tst, atol=1e-6)
+    assert np.allclose(Hess_fwhm_ref.flatten()[[0,1,3]], 
+                       Hess_fwhm_tst, atol=1e-6)
+
 
